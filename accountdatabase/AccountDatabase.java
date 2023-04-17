49914a356c8de2b237e80fc25eb852e19d261ea5
Grant Daniel Fugate <77984166+gfugate@users.noreply.github.com>
2023-04-17 09:54:41 -0500

    AccountDatabase changes

--- accountdatabase/AccountDatabase.java
+++ accountdatabase/AccountDatabase.java
@@ -1,16 +1,17 @@
 package accountdatabase;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Properties;
 
 public class AccountDatabase {
     private Connection conn;
 
-    public AccountDatabase() {
+    public AccountDatabase() throws IOException {
     	
     	Properties properties = new Properties();
-		FileInputStream fileInputStream = new FileInputStream("lab7out/db.properties");
+		FileInputStream fileInputStream = new FileInputStream("accountdatabase/db.properties");
 		properties.load(fileInputStream);
 		
 		String url = properties.getProperty("url");
